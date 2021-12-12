27b2b523665f307e611f1e7a72e1dfc8ddc4e756
Dominic Oludare <dominicoludare@gmail.com>
2021-12-12 16:36:09 +0100

    navlink onclick to blue

--- src/components/navBar/index.js
+++ src/components/navBar/index.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom';
 import './index.scss'
 
+
+
 const NavBar =()=> {
+    const [grey, setGrey] = useState(true);
+
+    const greyLink = {
+        padding: "5px 11px",
+        color: "#A5B4CB",
+        background: "#E4E9EF",
+        fontFamily: "sans-serif",
+        marginRight: "7.5px",
+        borderRadius: "3px"
+    }
+
+    const blueLink = {
+        padding: "5px 11px",
+        color: "white",
+        background: "#006AFF",
+        fontFamily: "sans-serif",
+        marginRight: "7.5px",
+        borderRadius: "3px"
+    }
+
+    const changeColor=()=> {
+        setGrey(!grey)
+    }
 
     return(
 
@@ -11,20 +36,20 @@ const NavBar =()=> {
                 <div className="nav-sub">
                     <ul>
                         <li>
-                            <NavLink to={'/'} exact 
-                            // activeStyle={{color: 'white', padding: "5px 11px", background: '#006AFF', borderRadius: '3px', marginRight: '4px',}} 
-                            ><span className="one">1</span></NavLink> 
+                            <NavLink className="activeOne" to={'/'} exact 
+                            activeStyle={{color: 'white', padding: "5px 11px", background: '#006AFF', borderRadius: '3px', marginRight: '7.5px'}}  
+                            ><span className="one" >1</span></NavLink> 
                             <span className="navbar-hidden"><NavLink to={'/'} exact activeStyle={{color: 'black'}} > Verify Account</NavLink></span>
                         </li>
                         <li>
-                            <NavLink to={'/social'} exact 
-                            // activeStyle={{color: 'white', padding: "5px 11px", marginRight: "10px", background: '#006AFF', borderRadius: '3px',}} 
+                            <NavLink className="activeTwo" to={'/social'} exact 
+                            activeStyle={{color: 'white', padding: "5px 7.5px", marginRight: "7.5px", background: '#006AFF', borderRadius: '3px'}} 
                             ><span className="two">2</span>&nbsp;</NavLink> 
                             <span className="navbar-hidden"><NavLink to={'/social'} exact activeStyle={{color: 'black'}} >Social Handles</NavLink></span>
                         </li>
                         <li>
-                            <NavLink to={'/business'} exact 
-                           // activeStyle={{color: 'white', padding: "5px 11px", background: '#006AFF', borderRadius: '3px', marginRight: '10px'}} 
+                            <NavLink className="activeThree" to={'/business'} exact 
+                           activeStyle={{color: 'white', padding: "5px 7.5px", background: '#006AFF', borderRadius: '3px', marginRight: '7.5px'}} 
                             ><label className="three">3</label>&nbsp;</NavLink> 
                             <span className="navbar-hidden"><NavLink to={'/business'} exact activeStyle={{color: 'black'}} >Business Category</NavLink></span>
                         </li>
@@ -36,3 +61,4 @@ const NavBar =()=> {
 }
 
 export default NavBar;
+
